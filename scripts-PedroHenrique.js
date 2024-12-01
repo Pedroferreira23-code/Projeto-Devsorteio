a420703a141560a542ddc52707b8740307e0b71b
Pedroferreira23 <pedroferreiran11@gmail.com>
2024-12-01 18:06:45 -0300

    Reformulação no código HTML CSS JAVASCRIPT

--- scripts-PedroHenrique.js
+++ scripts-PedroHenrique.js
@@ -1,9 +1,19 @@
+const buttonConsult = document.querySelector(".btn")
+const result = document.querySelector(".result")
+
 function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
 
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.floor(document.querySelector(".input-max").value)
+    if (min >= max) {
+        alert("Valor mínimo deve ser menor que o valor máximo");
+    }
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-     alert(result)
+    else {
+        const result = Math.floor(Math.random() * (max - min) + min);
+        document.querySelector(".result").value = result;
+    }
+    // alert(result)
 }
+
+buttonConsult.addEventListener("click", generateNumber);
